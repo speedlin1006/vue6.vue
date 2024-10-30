9da47a0f7eef9f01d1ad5a1cfbf707916c344f25
speedlin1006 <speedlin10060106@gmail.com>
2024-10-30 09:15:14 +0800

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue';
 import vuetify from 'vite-plugin-vuetify';
 
 export default defineConfig({
-  base: '/vue6.vue/', // 使用 GitHub Pages 專案路徑
+  base: '/vue6.vue/', // 使用 GitHub Pages 的專案路徑
   plugins: [vue(), vuetify()],
 });
