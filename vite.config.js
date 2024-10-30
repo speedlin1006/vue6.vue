5de30c74f092e8b0f1f0d1d4111cc47ad536507e
speedlin1006 <speedlin10060106@gmail.com>
2024-10-30 11:57:26 +0800

    Update vite.config.js with correct base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue';
 import vuetify from 'vite-plugin-vuetify';
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/vue6.vue/' : '/', // 本地為 '/'，部署為 '/vue6.vue/'
+  base: '/vue6.vue/',
   plugins: [vue(), vuetify()],
 });
